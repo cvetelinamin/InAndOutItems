db301bb6e294afccee659074daca95c114b54bdd
cvetelinamin <cvetelina.min.georgieva@gmail.com>
2021-09-18 22:49:26 +0300

    Added delete and update, validation for expense

---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Expense")]
+        [Required]
+        public string ExpenseName { get; set; }
+
+        [Range(1, 1000000, ErrorMessage = "Amount must be greater than 0")]
+        public decimal Amount { get; set; }
+    }
+}
